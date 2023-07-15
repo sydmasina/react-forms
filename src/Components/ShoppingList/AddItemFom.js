bdbcf2637e859bb7ee7e9fe24a8d510a8e91666a
sydmasina <masinasydwell@gmail.com>
2023-07-15 12:19:52 +0200

    Update form

--- src/Components/ShoppingList/AddItemFom.js
+++ src/Components/ShoppingList/AddItemFom.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AddItemForm extends Component{
     constructor(props){
         super(props);
-        this.state = {name: "", qty: 0}
+        this.state = {name: "", qty: ""}
         this.handleInputChange = this.handleInputChange.bind(this)
         this.handleAddItem = this.handleAddItem.bind(this);
     }
@@ -13,17 +13,18 @@ class AddItemForm extends Component{
         })
     }
     handleAddItem(evt){
-        evt.preventDefault()
-        this.props.addItem(evt, this.state)
+        evt.preventDefault();
+        this.props.addItem(evt, this.state);
+        this.setState({ name: "", qty: ""});
     }
     render(){
         return (
             <div>
                 <form>
                 <label htmlFor='name'>Name: </label>
-                <input onChange={this.handleInputChange} type='text' name='name' id='name' />
+                <input value={this.state.name} onChange={this.handleInputChange} type='text' name='name' id='name' />
                 <label htmlFor='qty'> Qty: </label>
-                <input onChange={this.handleInputChange} type='number' name='qty' id='qty' />
+                <input value={this.state.qty} onChange={this.handleInputChange} type='number' name='qty' id='qty' />
                 <button onClick={this.handleAddItem}>Add item</button>
                 </form>
             </div>
